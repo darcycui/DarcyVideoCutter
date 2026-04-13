9354252275d126a1b1c3111b1dd5c9fd09a908fc
Tom_docker <Tom_docker@jerry.com>
2026-04-13 17:07:17 +0800

    build: v1.0.8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.darcy.videocutter"
         minSdk = 26
         targetSdk = 35
-        versionCode = 7
-        versionName = "1.0.7"
+        versionCode = 8
+        versionName = "1.0.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
